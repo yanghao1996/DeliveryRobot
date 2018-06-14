a936b0919df5f584f686f0fbd0c96348aaa903b0
tsaikenyoo <tsaikenyoo@gmail.com>
2018-06-14 22:43:10 +0800

    add log/Logger

---
+++ log/Logger.h
@@ -0,0 +1,35 @@
+// Logger log important informations
+#ifndef LOGGER_H
+#define LOGGER_H
+
+#include <iostream>
+#include <string>
+#include <chrono>
+#include <iomanip>
+#include <ctime>
+#include <vector>
+using namespace std;
+
+enum LogLevel {
+    ERROR,
+    INFO
+};
+
+const vector<string> logStrMap{"ERROR", "INFO"};
+
+class Logger {
+public:
+    static void info(string message) {
+        Logger::printLog(INFO, message);
+    }
+private:
+    static void printLog(LogLevel level, string message) {
+        auto now = chrono::system_clock::now();
+        auto now_c = std::chrono::system_clock::to_time_t(now);
+        cout << put_time(std::localtime(&now_c), "%c") 
+             << " [" << logStrMap[level] << "]: " << message << endl;
+    }
+};
+
+
+# endif //LOGGER_H
