402db65c07ece985d9d27b56bec577fae78adaea
tsaikenyoo <tsaikenyoo@gmail.com>
2018-06-21 10:14:25 +0800

    add some methods to RobotController

--- robot_control/RobotController.cpp
+++ robot_control/RobotController.cpp
@@ -1,4 +1,8 @@
 #include "RobotController.h"
+#include "../log/Logger.h"
+#include <math.h>
+#include <string>
+using namespace std;
 
 RobotController::RobotController() {
     this->serial = new Serial();
@@ -9,10 +13,57 @@ RobotController::~RobotController() {
     this->serial->closePort();
 }
 
+void RobotController::setRotate(int angle) {
+    if (angle < -40 || angle > 40) {
+        Logger::error("invalid angle value!");
+        exit(1);
+    }
+    this->motorCommand.isRotate = true;
+    this->motorCommand.rotateAngle = angle;
+}
+
+void RobotController::setStraight(unsigned int distance) {
+    if (distance > 100) {
+        Logger::error("invalid distance value!");
+        exit(1);
+    }
+    this->motorCommand.isRotate = false;
+    this->motorCommand.distance = distance;
+} 
+
+const char* RobotController::compile() {
+    string compiledStr = "000";
+    if (this->motorCommand.isRotate == false) {
+        compiledStr[0] = '1';
+        if(this->motorCommand.rotateAngle < 0) {
+            compiledStr[1] = '1';
+        }
+        if(abs(this->motorCommand.rotateAngle) == 90) {
+            compiledStr[2] = '1';
+        }
+    }
+    else {
+        int secondNum = this->motorCommand.distance / 16;
+        compiledStr[1] = intToHexStr(secondNum);
+        int thirdNum = this->motorCommand.distance - 16 * secondNum;
+        compiledStr[2] = intToHexStr(thirdNum);
+    }
+    return compiledStr.c_str();
+}
+
 void RobotController::sendCommand(const char* command) {
     this->serial->writePort(command);
 }
 
 void RobotController::readMessage(char* readBuffer) {
     this->serial->readPort(readBuffer);
+}
+
+char intToHexStr(int num) {
+    if (num < 10) {
+        return '0' + num;
+    }
+    else {
+        return 'A' + num - 10;
+    }
 }
--- robot_control/RobotController.h
+++ robot_control/RobotController.h
@@ -3,14 +3,45 @@
 
 #include "Serial.h"
 
+/* struct Command stores the commands sent to motor,
+ * there are 2 modes, rotate and driving straight.
+ * for rotate, angle can be (-)60 or (-)90,
+ * for straight, distance ranges from 1(dm) to 100(dm) (integer)
+ * */
+struct Command {
+    bool isRotate;
+    int rotateAngle;
+    unsigned int distance; 
+};
+
 class RobotController {
 private:
     Serial* serial;
+    struct Command motorCommand;
 public:
     RobotController();
     ~RobotController();
+    // set command to rotate mode
+    void setRotate(int angle); 
+    // set command to straight mode
+    void setStraight(unsigned int distance);
+    /* compile motorCommand into sequence
+     * for rotate, 3 chars are used, e.g. 000
+     * - 1st: 0 for rotate, 1 for straight
+     * - 2nd: 0 for clockwise, 1 for anticlockwise
+     * - 3rd: angle value, 0 for 60, 1 for 90
+     * 
+     * for straight 2 chars are used, e.g. 100
+     * - 1st: 0 for rotate, 1 for straight
+     * - 2nd: distance, ranges from 1 to (A)10 
+     */
+    const char* compile();
+    // send compiled command to serial
     void sendCommand(const char* command);
+    // read message from serial
     void readMessage(char* readBuffer);
 };
 
+// convert single int value(0 to 15) to char
+char intToHexStr(int num);
 #endif // ROBOTCONTROLLER_H
